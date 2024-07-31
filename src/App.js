bb9ba637573c276a9e9aa29d8bbc55c2e520020e
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-07-31 10:42:18 +0530

    parmsuse

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@ import "./App.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Link } from "react-router-dom";
 import Page2 from "../src/page/Page2";
 import Page1 from "../src/page/Page1";
 import Hero from "./componets/Hero";
 import About from "./componets/About";
-import Form from "./componets/Form";
-import ContactUs from "./componets/ContactUs";
-import Api from "./componets/Api";
 function App() {
   return (
     <div className="App">
-      
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/" element={<About />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          
-        </Routes>
-        <Form/> 
-        <ContactUs/>
-        <Api/>
-       
-     
+      <Routes>
+        
+        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<About />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </div>
   );
 }
--- src/componets/Data.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-export const DataFix = createContext();
-
-export default function CombineData({ children }) {
-  const name = "Vishal";
-  const value = { name };
-  return <DataFix.Provider value={value}>{children}</DataFix.Provider>;
-}
